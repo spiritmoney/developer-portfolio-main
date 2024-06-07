f2deeb49bbce8792654340d912d5532f161fe61f
Kingsley Odim <kingodim@outlook.com>
2024-06-07 19:21:25 +0100

    third commit

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -3,8 +3,8 @@ export const contactsData = {
   phone: "+2348100129769",
   address: "Lagos, Nigeria ",
   github: "https://github.com/spiritmoney",
-  linkedIn: "https://www.linkedin.com/in/spiiiritmoney/",
-  twitter: "https://twitter.com/spiiiritmoneyko",
+  linkedIn: "https://www.linkedin.com/in/spiritmoney/",
+  twitter: "https://twitter.com/spiritmoneyko",
   stackOverflow: "https://stackoverflow.com/users/22843947/kingsley-odim",
   devUsername: "spiritmoney",
 };
